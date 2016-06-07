af38e953550747e7800bce13bc1a70a2e3b71b22
Jesse Showalter <iamjesseshowalter@gmail.com>
2016-06-06 19:26:35 -1000

    Lots of changes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -15,20 +15,6 @@ $(document).ready(function(){
   });
 
 
-
-  // Initiate the unslider
-  $('.banner').unslider({
-    infinite: true,
-    autoplay: true,
-    speed:    1200,
-    arrows:   false,
-    delay:    8000
-  });
-  $('[data-remodal-id=1000]').remodal({
-    modifier: 'with-red-theme'
-  });
-
-
   // --------------------------------------------
   // Hover for products (dim all other products on hover)
   // --------------------------------------------
@@ -64,49 +50,13 @@ $(document).ready(function(){
 
   });
 
-  // --------------------------------------------
-  // overlay navigation
-  // --------------------------------------------
-  (function() {
-  	var triggerBttn = document.getElementById( 'trigger-overlay' ),
-  		overlay = document.querySelector( 'div.overlay' ),
-  		closeBttn = overlay.querySelector( 'button.overlay-close' );
-  		transEndEventNames = {
-  			'WebkitTransition': 'webkitTransitionEnd',
-  			'MozTransition': 'transitionend',
-  			'OTransition': 'oTransitionEnd',
-  			'msTransition': 'MSTransitionEnd',
-  			'transition': 'transitionend'
-  		},
-  		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-  		support = { transitions : Modernizr.csstransitions };
-
-  	function toggleOverlay() {
-  		if( classie.has( overlay, 'open' ) ) {
-  			classie.remove( overlay, 'open' );
-  			classie.add( overlay, 'close' );
-  			var onEndTransitionFn = function( ev ) {
-  				if( support.transitions ) {
-  					if( ev.propertyName !== 'visibility' ) return;
-  					this.removeEventListener( transEndEventName, onEndTransitionFn );
-  				}
-  				classie.remove( overlay, 'close' );
-  			};
-  			if( support.transitions ) {
-  				overlay.addEventListener( transEndEventName, onEndTransitionFn );
-  			}
-  			else {
-  				onEndTransitionFn();
-  			}
-  		}
-  		else if( !classie.has( overlay, 'close' ) ) {
-  			classie.add( overlay, 'open' );
-  		}
-  	}
-
-  	triggerBttn.addEventListener( 'click', toggleOverlay );
-  	closeBttn.addEventListener( 'click', toggleOverlay );
-  })();
+// --------------------------------------------
+// overlay navigation
+// --------------------------------------------
+  $('#toggle').click(function() {
+    $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+  });
 
 
 }); // end document.ready
